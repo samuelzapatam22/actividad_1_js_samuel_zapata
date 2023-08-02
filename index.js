ae92c070c0c4c34e5b18aec8ad3b445291af5758
unknown <samuelzapatam1@gmail.com>
2023-08-01 19:30:00 -0500

    Actividad 1 JS

---
+++ index.js
@@ -0,0 +1,8 @@
+let Name,LastName, BirthDay,City,Age;
+Name = 'Samuel';
+LastName = 'Zapata';
+BirthDay = 2004;
+City = 'Medellin';
+Age = 18;
+console.log("Mi Nombre es",Name,LastName,",vivo en la Ciudad de la eterna Primavera,", City ,".Naci en el año",BirthDay,",por ende Tengo",Age,"años." );
+
